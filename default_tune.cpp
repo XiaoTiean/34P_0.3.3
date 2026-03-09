91d527cd151098cf966bcfd4a1ca75e0c590d582
XiaoTiean <124254796+XiaoTiean@users.noreply.github.com>
2026-03-10 00:58:23 +0700

    Update default_tune.cpp

--- default_tune.cpp
+++ default_tune.cpp
@@ -1,5 +1,14 @@
 #include "pch.h"
 
 void boardTuneDefaults() {
-    engineConfiguration->displacement = 1.6;
+	engineConfiguration->displacement = 0.15; // 100cc
+	engineConfiguration->cylindersCount = 1;
+	engineConfiguration->firingOrder = FO_1;
+
+	/**
+	 * 12/1 trigger
+	 */
+	engineConfiguration->trigger.type = trigger_type_e::TT_TOOTHED_WHEEL;
+	engineConfiguration->trigger.customTotalToothCount = 12;
+	engineConfiguration->trigger.customSkippedToothCount = 1;
 }
